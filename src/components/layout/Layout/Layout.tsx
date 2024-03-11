931a61e636ce33aff4a14b3b959fcb83d3be47a6
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-03-11 18:56:20 +0400

    font updates + ios bug fixes

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import clsx from 'clsx';
 
 import Footer from '@/components/layout/Footer';
@@ -12,53 +11,13 @@ export const metadata: Metadata = {
     'Blockchain game for cryptocurrency stakes strategic PvP gameplay and full transparency',
 };
 
-const coreSansC = localFont({
-  src: [
-    {
-      path: '../../../fonts/CoreSansC-55Medium.woff',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/CoreSansC-55Medium.woff',
-      weight: '500',
-      style: 'normal',
-    },
-  ],
-  display: 'swap',
-  variable: '--core-sans-c',
-});
-
-const junegull = localFont({
-  src: [
-    {
-      path: '../../../fonts/junegull.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../../fonts/junegull.woff',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  display: 'swap',
-  variable: '--junegull',
-});
-
 const Layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const fontVariables = clsx([
-    coreSansC.variable,
-    junegull.variable,
-    styles.root,
-  ]);
-
   return (
-    <div className={fontVariables}>
+    <div className={styles.root}>
       <header>HEADER</header>
       <main>{children}</main>
       <Footer />
