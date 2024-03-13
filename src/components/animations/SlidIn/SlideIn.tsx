57786575283a6f7538645507113ac98f87765b3d
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-03-13 17:57:34 +0400

    Animations update

---
+++ src/components/animations/SlidIn/SlideIn.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import { motion } from 'framer-motion';
+
+interface ISlideInProps {
+  children: React.ReactNode;
+  className?: string;
+  duration?: number;
+  initialScale?: number;
+}
+
+const SlideIn: FC<ISlideInProps> = ({
+  children,
+  className,
+  initialScale,
+  duration,
+}) => {
+  const animationDuration = duration ? duration : 1;
+  const animationInitialScale = initialScale ? initialScale : 0.8;
+
+  return (
+    <motion.div
+      className={className}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{
+        duration: animationDuration,
+        ease: 'easeInOut',
+      }}
+      variants={{
+        hidden: { opacity: 0, y: '2em' },
+        visible: { opacity: 1, y: '0em' },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default SlideIn;
---
+++ src/components/animations/ZoomIn/ZoomIn.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import { motion } from 'framer-motion';
+
+interface IZoomInProps {
+  children: React.ReactNode;
+  className?: string;
+  duration?: number;
+  initialScale?: number;
+}
+
+const ZoomIn: FC<IZoomInProps> = ({
+  children,
+  className,
+  initialScale,
+  duration,
+}) => {
+  const animationDuration = duration ? duration : 0.6;
+  const animationInitialScale = initialScale ? initialScale : 0.8;
+
+  return (
+    <motion.div
+      className={className}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{
+        duration: animationDuration,
+        ease: 'easeInOut',
+      }}
+      variants={{
+        hidden: { scale: animationInitialScale },
+        visible: { scale: 1 },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default ZoomIn;
